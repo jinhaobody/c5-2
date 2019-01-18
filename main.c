21f0d2222981935140fc55a0fc7e03c7469f701b
zhaojinhao <2481406659@qq.com>
2019-01-18 09:58:47 +0800

    结构框架

--- main.c
+++ main.c
@@ -1,9 +1,55 @@
 #include <stdio.h>
+struct student
+{
+    int yuwen;
+    int shuxue;
+    int yingyu;
+    char xingming[100];
+};
+int main()
+{
+    int index = 0;
+    struct student arr[100];
+    printf("huanyingjinruxueshengchengjiguanlixitong!\n");
 
-int main(){
+    while (1)
+    {
+        printf("1---\n");
+        printf("2---\n");
+        printf("3---\n");
+        printf("4---\n");
+        printf("5---\n");
+        printf("6---\n");
+        printf("7---\n");
+        printf("8---\n");
 
-    printf("hello world!");
-
+        printf("请选择相应功能\n");
+        int code;
+        scanf("%d", &code);
+        if (code == 1)
+        {
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+        }
+        if (code == 6)
+        {
+        }
+        if (code == 7)
+        {
+        }
+        if (code == 8)
+        {
+        }
+    }
     return 0;
-
 }
